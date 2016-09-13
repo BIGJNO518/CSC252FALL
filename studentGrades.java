236188513c1a41a647910e88f999c83d6915f87f
Jason P Nowak <BIGJNO518@users.noreply.github.com>
2016-09-13 10:55:37 -0400

    Update studentGrades.java

--- studentGrades.java
+++ studentGrades.java
@@ -1,89 +1,34 @@
 import java.io.*;
 import java.util.*;
-
-public class studentGrades 
+public class studentGrades
 {
-	//Please ignore this part of the program, it was just to see if my methods were working. SO
-	// I will import the correct file in class and rewrite for however many lines, etc. JN
 	public static void main(String[] args) throws IOException
 	{
-		
-		
-				
 		double grade1;
 		double grade2;
 		double grade3;
-		
-		
 		String letterGrade;
+		File file1 = new File("Test.txt");
+		Scanner inputFile = new Scanner(file1);
 		String studentName;
-		String studentGrades = null;
-		
-		int lineNumber = 0;
-		
-		File studentGradeRep = new File("studentGradeSheet.txt");
-		Scanner inputFile = new Scanner(studentGradeRep);
+		String finalLetterGrade;
+		PrintWriter outputFile = new PrintWriter("output.txt");
 		
 		while (inputFile.hasNext())
 		{
-			studentGrades = inputFile.nextLine();
-			lineNumber++;
-			System.out.println(lineNumber + " " + studentGrades +"\n");
+			studentName = inputFile.nextLine();
+			grade1 = inputFile.nextDouble();
+			grade2 = inputFile.nextDouble();
+			grade3 = inputFile.nextDouble();
+			finalLetterGrade = letterGrade(grade1, grade2, grade3);
+			outputFile.println(studentName);
+			outputFile.println(finalLetterGrade);
+			inputFile.nextLine();
 		}
-			do
-			{
-				studentName = "Jason N";
-				grade1 = 85;
-				grade2 = 92;
-				grade3 = 86;
-				letterGrade = letterGrade(grade1, grade2, grade3);
-				System.out.println("Letter grade for " + studentName + " is " + letterGrade + ".");
-				lineNumber++;
-			}
-			while (lineNumber ==2);
-			
-			
-			do
-			{
-				studentName = "Sean O";
-				grade1 = 98;
-				grade2 = 93;
-				grade3 = 97;
-				letterGrade = letterGrade(grade1, grade2, grade3);
-				System.out.println("Letter grade for " + studentName + " is " + letterGrade + ".");
-				lineNumber++;
-			}
-			while (lineNumber ==4);
-			
-			do
-			{
-				studentName = "Jesse C";
-				grade1 = 85;
-				grade2 = 75;
-				grade3 = 70;
-				letterGrade = letterGrade(grade1, grade2, grade3);
-				System.out.println("Letter grade for " + studentName + " is " + letterGrade + ".");
-				lineNumber++;
-			}
-			while (lineNumber ==6);
-
-			do
-			{
-				studentName = "Extra S";
-				grade1 = 60;
-				grade2 = 65;
-				grade3 = 61;
-				letterGrade = letterGrade(grade1, grade2, grade3);
-				System.out.println("Letter grade for " + studentName + " is " + letterGrade + ".");
-				lineNumber++;
-			}
-			while (lineNumber ==8);
+		outputFile.close();
 	}
-	
 
-
-
-	/**
+	 /**
 	 * 
 	 * @param g1 The first grade in double.
 	 * @param g2 The second grade in double.
@@ -92,39 +37,39 @@ public class studentGrades
 	 */
 	public static String letterGrade(double g1, double g2, double g3)
 	{
-		double finalNumGrade;
+		double finalnumGrade;
 		String finalGrade;
-		finalNumGrade = (g1 * 0.2) + (g2 * 0.3) + (g3 * 0.5);
+		finalnumGrade = (g1 * 0.2) + (g2 * 0.3) + (g3 * 0.5);
 		
-		if(finalNumGrade >= 92)
+		if(finalnumGrade >= 92)
 		{
 			finalGrade = "A";
 		}
-		else if(finalNumGrade >= 90)
+		else if(finalnumGrade >= 90)
 		{
 			finalGrade = "A-";
 		}
-		else if(finalNumGrade >= 88)
-		{
-			finalGrade = "B+";
-		}
-		else if(finalNumGrade >= 82)
-		{
-			finalGrade = "B";
-		}
-		else if(finalNumGrade >= 80)
+		else if(finalnumGrade >= 88)
 		{
 			finalGrade = "B-";
 		}
-		else if(finalNumGrade >= 78)
+		else if(finalnumGrade >= 82)
+		{
+			finalGrade = "B";
+		}
+		else if(finalnumGrade >= 80)
+		{
+			finalGrade = "B-";
+		}
+		else if(finalnumGrade >= 78)
 		{
 			finalGrade = "C+";
 		}
-		else if(finalNumGrade >= 70)
+		else if(finalnumGrade >= 70)
 		{
 			finalGrade = "C";
 		}
-		else if(finalNumGrade >= 60)
+		else if(finalnumGrade >= 60)
 		{
 			finalGrade = "D";
 		}
@@ -135,19 +80,4 @@ public class studentGrades
 		
 		return finalGrade;
 	}
-	/**
-	 * 
-	 * @param sn The student's name.
-	 * @param lg The letter grade the student will get. 
-	 * @throws IOException
-	 */
-	public static void outputFile(String sn, String lg) throws IOException
-	{
-		PrintWriter outputFile = new PrintWriter("output.txt");
-		outputFile.println(sn);
-		outputFile.println(lg);
-		outputFile.close();
-	}
-	
 }
-
