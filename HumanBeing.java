da32ff2543c8a855a07bcd766162de19e55ec451
Jason P Nowak <BIGJNO518@users.noreply.github.com>
2016-10-04 10:59:56 -0400

    Add files via upload

---
+++ HumanBeing.java
@@ -0,0 +1,30 @@
+/** Jason Nowak 
+ * CSC 252
+ * Dr. Kim Gero
+ * 10/04/2016
+ * @author nowakj943
+ *
+ */
+
+public class HumanBeing 
+{
+
+	private String name;
+	private int age;
+	private double height;
+	private double weight;
+	private String gender;
+	private String hairColor;
+	
+	
+	public HumanBeing () // default constructor
+	{
+		
+		
+		
+		
+	}
+
+	
+
+}
